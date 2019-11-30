d6ec1f7a5826e8711f10b39579a2529b590639bb
Anil Patidar <42931974+AnilNITT@users.noreply.github.com>
2019-11-30 09:31:46 +0530

    Add files via upload

---
+++ Square Shape/Square.py
@@ -0,0 +1,19 @@
+print square like this
+
+*  *  *  *  *  *  * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*  *  *  *  *  *  * 
+
+============================================
+
+for row in range(7):
+    for col in range(7):
+        if(row==0 or row==6 or col==0 or col==6):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()   #print next line or row
